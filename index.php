1a0f834139602f7ac4463c8f1d4a62eb4b232538
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-17 20:48:02 +0300

    galim vaiksciot po puslapius pasiimdami ju id numeri

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <?php
 include 'classes/db.php';
 
+$page_id = isset($_GET['page']) ? $_GET['page'] : 1; // if funkcijos sutrumpinimas
+
+/* if (isset($_GET['page'])) {
+	$page_id = $_GET['page'];
+} else {
+	$page_id = 1;
+} */
+
 $db = new DB();
 $result = $db->query("SELECT * FROM pages WHERE id = '1'");
 
