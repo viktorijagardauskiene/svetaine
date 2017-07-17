58b090da56ba29f4fa8a7fd5378a4b9c8f713bd3
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-17 20:14:02 +0300

    cunstruct ir destruct prisijungimai prie db sukurti

--- db.php
+++ db.php
@@ -4,14 +4,15 @@ class DB {
 	private $db_username = 'ViktorijaG';
 	private $db_password = 'kashtankai15';
 	private $db = 'viktorijag';
+	private $conn;
 
 	//prisijungiam prie db
 	function __construct() {
 		try {
-			    $conn = new PDO("mysql:host=$this->db_hostname;dbname=$this->db", $this->db_username, $this->db_password);
+			    $this->conn = new PDO("mysql:host=$this->db_hostname;dbname=$this->db", $this->db_username, $this->db_password);
 			    // set the PDO error mode to exception
-			    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			   // echo "Connected successfully"; <<- nenaudojam nes viskas veikia
+			   $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			   //echo "Connected successfully";
 		    }
 		catch(PDOException $e) {
 		    	echo "Connection failed: " . $e->getMessage();
@@ -19,14 +20,22 @@ class DB {
 	}
 
 	// ivykdom koda/uzklausa ir grazinam reiksmes
+	public function query($sql) {
+		$query = $this->conn->query($sql);
+		$query->execute();
+		$result = $query->fetchAll();
+		return $result;
+
+	}
+
+
 
 
 	// ir atsijungiam nuo db
 	function __destruct() {
-
+		$this->conn = null;
 	}
 
 
 }
 
-$db = new DB();
